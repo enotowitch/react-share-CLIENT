0121dba49d63312a60323ff094fc5a90ce0efd6a
enotowitch <en.enotowitch@gmail.com>
2023-06-02 15:49:15 +0300

    4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import SocialMediaButtons from './components/SocialMediaButtons';
 import HelmetMetaData from './components/HelmetMetaData';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
+
+	const [state, stateSet] = useState(0)
+
+	useEffect(() => {
+		stateSet(prev => prev + 1)
+	}, [])
+
 	return (
 		<>
 			<HelmetMetaData
 				title="123"
 				image="https://www.imgonline.com.ua/examples/random-pixels-big.png"
+				state={state}
 			/>
 			<SocialMediaButtons />
 		</>
